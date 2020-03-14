394229f100e6d88b376c06086f14c2d36a6afefa
Ahmad Guenoun <amguenoun@gmail.com>
2020-03-14 19:54:43 -0400

    created preliminary monster class

---
+++ src/monster.py
@@ -0,0 +1,16 @@
+class Monster:
+    def __init__(self, name, description, room, health, skill):
+        self.name = name
+        self.description = description
+        self.room = room
+        self.health = health
+        self.skill = skill
+
+    def on_death(self):
+        print(f'{self.name} has died!')
+
+    def on_damage(self, dmg):
+        if health - dmg <= 0:
+            self.on_death
+        else:
+            health -= dmg
